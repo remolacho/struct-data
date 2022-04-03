95cd1189484adb9a4b95b571bf46a6b074083cb5
remolacho <jonathang_25@gmail.com>
2022-04-02 22:14:20 -0500

    se crean listas

--- list/list.go
+++ list/list.go
@@ -4,65 +4,142 @@ import "fmt"
 
 type List struct{}
 
-type Item struct {
-	name     string
-	quantity int
-	next     *Item
+type Node struct {
+	Name     string
+	Quantity int
+	Next     *Node
 }
 
-var root *Item = nil
+var root *Node = nil
 
 func New() *List {
 	return &List{}
 }
 
 func (l *List) All() {
-	var item *Item = root
+	var item *Node = root
 
 	for i := 1; item != nil; i++ {
-		fmt.Printf("%d- Article: %s, quantity: %d \n", i, item.name, item.quantity)
-		item = item.next
+		fmt.Printf("%d- Article: %s, quantity: %d \n", i, item.Name, item.Quantity)
+		item = item.Next
 	}
 }
 
 func (l *List) Size() int {
-	var item *Item = root
+	var item *Node = root
 	index := 1
 
 	if item == nil {
 		return 0
 	}
 
-	for item.next != nil {
+	for item.Next != nil {
 		index++
-		item = item.next
+		item = item.Next
 	}
 
 	return index
 }
 
-func (l *List) Add(name string, quantity int) {
-	var item Item
-	item.name = name
-	item.quantity = quantity
-	item.next = nil
+func (l *List) Item(name string, quantity int) Node {
+	return Node{
+		Name:     name,
+		Quantity: quantity,
+		Next:     nil,
+	}
+}
 
+func (l *List) Add(item Node) {
 	if root == nil {
 		root = &item
 		return
 	}
 
-	var pivot *Item
+	var pivot *Node
 	pivot = root
 
 	for {
-		if pivot.next == nil {
-			pivot.next = &item
+		if pivot.Next == nil {
+			pivot.Next = &item
 			break
 		}
 
-		pivot = pivot.next
+		pivot = pivot.Next
 	}
+}
+
+func (l *List) Push(item Node, index int) {
+	var position int = 1
+	var pivot *Node
+
+	if index < position || index > l.Size() {
+		return
+	}
+
+	if index == position {
+		item.Next = root
+		root = &item
+		return
+	}
+
+	pivot = root
+
+	for {
+		// -1 hace que se salga una en el item anterior
+		if position == index-1 {
+			break
+		}
+
+		pivot = pivot.Next
+		position++
+	}
+
+	item.Next = pivot.Next
+	pivot.Next = &item
+}
+
+func (l *List) Find(index int) Node {
+	item := root
+	position := 1
+
+	if index < position || index > l.Size() {
+		return Node{}
+	}
+
+	for {
+		if index == position {
+			break
+		}
+
+		item = item.Next
+		position++
+	}
+
+	return *item
+}
+
+func (l *List) First() Node {
+	if l.Size() == 0 {
+		return Node{}
+	}
+
+	return *root
+}
+
+func (l *List) Last() Node {
+	item := root
+	position := 1
+
+	if l.Size() == position {
+		return *item
+	}
+
+	for position < l.Size() {
+		item = item.Next
+		position++
+	}
+
+	return *item
 }
 
 func (l *List) Delete(index int) {
@@ -73,26 +150,26 @@ func (l *List) Delete(index int) {
 	}
 
 	if index == positionRoot {
-		root = root.next
+		root = root.Next
 		return
 	}
 
 	destroyItem(index, positionRoot+1, root)
 }
 
-func destroyItem(index int, position int, item *Item) {
+func destroyItem(index int, position int, item *Node) {
 	if position == index {
-		if item.next.next == nil {
-			item.next = nil
+		if item.Next.Next == nil {
+			item.Next = nil
 		} else {
-			item.next = item.next.next
+			item.Next = item.Next.Next
 		}
 
 		return
 	}
 
 	if item != nil {
-		item = item.next
+		item = item.Next
 	}
 
 	destroyItem(index, position+1, item)
